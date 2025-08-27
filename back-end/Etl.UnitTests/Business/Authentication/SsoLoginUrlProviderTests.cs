2e3258f67d9f2bf22edda4463728915a6283e1b5
amirata051 <amirata.gh.m.n@gmail.com>
2025-08-27 22:39:27 +0330

    add test

--- back-end/Etl.UnitTests/Business/Authentication/SsoLoginUrlProviderTests.cs
+++ back-end/Etl.UnitTests/Business/Authentication/SsoLoginUrlProviderTests.cs
@@ -3,12 +3,30 @@ using Etl.Business.Authentication.Abstractions;
 
 namespace Etl.UnitTests.Business.Authentication;
 
-internal class SsoLoginUrlProviderTests
+public class SsoLoginUrlProviderTests
 {
-	private readonly ISsoLoginUrlProvider _sut;
+    private readonly ConfigurationManager _configuration;
+    private readonly ISsoLoginUrlProvider _sut;
 
 	public SsoLoginUrlProviderTests()
 	{
+		_configuration = new ConfigurationManager();
 		_sut = new SsoLoginUrlProvider();
+	}
+
+	[Fact]
+	public void GetSsoLoginUrl_ShouldReturnConfigUrlConfiguredUrl_Whenever()
+	{
+        // Arrange
+        var ssoUrl = "fakeurl.com";
+        _configuration["SsoLoginUrl"] = ssoUrl;
+		var expected = new Uri(ssoUrl);
+
+		// Act
+		var actual = _sut.GetSsoLoginUrl();
+
+		// Assert
+		actual.
+
 	}
 }
